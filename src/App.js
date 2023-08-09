4b8e1619d7db3b69eec97f297462a543274086d8
SirHertzAlot <Dukjoseph@gmail.com>
2023-08-09 18:11:41 -0400

    Added state to and effect to our app to dynamically update quote categories.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Categories from "./components/Categories";
 import Quote from "./components/Quote";
+import {getCategories, getQuote} from "./quotesService";
 
 const App = () => {
+  const [categories, setCategories] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [quote, setQuote] = useState({});
+  
+  //Let's set our category so we can use it to fetch our quotes.
+  useEffect(() => {
+    getCategories().then(cats => {
+      if(cats.length > 0){
+        setCategories(cats);
+        setSelected(cats[0]);
+      }
+    });
+  }, []);
+
+  //When the 'selected' is set, fetch a quote
+  useEffect(() => {
+   selected && getQuote(selected).then(q => setQuote(q));
+  }, [selected]);
+
+  //Now let's fetch a new quote every 5 seconds.
+  useEffect(() => {
+    let timer = setInterval(() => {
+      getQuote(selected).then(q => setQuote(q));
+    }, 5000);
+    
+    return () => clearInterval(timer);
+  }, [selected])
+
   return (
     <div className="quote-master">
       <Categories
-        categories={["Brainy", "Funny"]}
-        selected={"Funny"}
-        onSelected={category => {}}
+        categories={categories}
+        selected={selected}
+        onSelected={category => setSelected(category)}
       />
-      <Quote
-        quote={"The first five days after the weekend are always the hardest."}
-        author={"A Lazy Cat"}
-      />
+      {quote && <Quote quote={quote.quote} author={quote.author} />}
     </div>
   );
 };
